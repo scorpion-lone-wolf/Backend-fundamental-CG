82798afc84c7f097a1ae2ec0e87c685a9aa7ae5d
CODECLOUDS-PURSOTAM <codeclouds-purosattam@CISPL-IO-751.local>
2025-10-09 23:00:22 +0530

    delete coding labs

--- week-6/coding-labs.sql
+++
@@ -1,6 +0,0 @@
--- 1. Write a SQL query that returns the id and name of every department,
--- sorted by the department’s name in ascending (alphabetical) order.
--- Your result should include all rows from the departments table.
-SELECT id, name FROM
-    departments d
-ORDER BY d.name ASC;
